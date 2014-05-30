22e6ce154dbf7e3838b23c4f04d47152b42302c7
puhrome <phirom.yim@fullsail.edu>
2014-05-29 20:59:28 -0400

    Functions
    Basic example of functions

---
+++ js/script.js
@@ -0,0 +1,30 @@
+/**
+ * Functions Created by puhrome on 5/29/14.
+ */
+//Functions Reusable Code
+//DRY Don't Repeat Yourself
+//They allow us to code once and be able to use again
+//Minimize errors
+//Function stores lines of code
+//Little factories
+//Reusability
+//Functions
+
+function functionName(){//basic structure is like conditional
+    //code the function runs
+}
+//javascript is c base
+
+//{ holds blocks of code}
+//(holds parameter)
+
+function outptMsg(){//function
+    console.log("Hello World");//block of code
+}
+
+function calcArea(){//function
+    var width = 20;//block of code
+    var height = 30;//block of code
+    var area = width * height;//block of code
+    console.log(area);//block of code
+}
